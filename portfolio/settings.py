82d160326d70723bddd9015d91e45b986b2ebedb
shraag <66220166+shraag@users.noreply.github.com>
2022-08-14 15:05:35 +0530

    changes settings to check db

--- portfolio/settings.py
+++ portfolio/settings.py
@@ -82,7 +82,7 @@ DATABASES = {
         'NAME': 'portfoliodb',
         'USER': 'postgres',
         'PASSWORD': 'django1234',
-        'HOST': 'localhost',
+        'HOST': '127.0.0.1',
         'PORT': '5432',
     }
 }
