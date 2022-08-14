c49a0846ff3bfe1494b50fe1ae588fad62bddc26
shraag <66220166+shraag@users.noreply.github.com>
2022-08-14 14:35:44 +0530

    added resume in static folder

--- portfolio/urls.py
+++ portfolio/urls.py
@@ -22,8 +22,8 @@ from django.conf.urls.static import static
 urlpatterns = [
     path('admin/', admin.site.urls),
     path('', jobs.views.home, name='home'),
-    path('jobs/<int:job_id>', jobs.views.detail, name='detail'),
-] 
+    path('projects/<int:job_id>', jobs.views.detail, name='detail'),
+]
 
 urlpatterns += static(settings.STATIC_URL, document_root = settings.STATIC_ROOT)
 urlpatterns += static(settings.MEDIA_URL, document_root = settings.MEDIA_ROOT)
